5dd0b35ce2112f10246a479be23799a435d2a921
caiotorrez <caiotorrez@live.com>
2018-11-01 11:35:12 -0300

    Base Project

---
+++ src/main/java/com/ipersonal/IPersonalApiApplication.java
@@ -0,0 +1,12 @@
+package com.ipersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPersonalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPersonalApiApplication.class, args);
+	}
+}
