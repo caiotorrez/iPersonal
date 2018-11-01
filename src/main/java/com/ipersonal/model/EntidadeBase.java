26acb44f87a57ea50175e5d30370699008e4b3c2
caiotorrez <caiotorrez@live.com>
2018-11-01 13:02:06 -0300

    Entidade Base Created

---
+++ src/main/java/com/ipersonal/model/EntidadeBase.java
@@ -0,0 +1,68 @@
+package com.ipersonal.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class EntidadeBase implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected Long id;
+	
+    @Column(columnDefinition = "boolean default true", nullable = false)
+    private boolean enabled = true;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EntidadeBase other = (EntidadeBase) obj;
+		if (enabled != other.enabled)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "EntidadeBase [id=" + id + ", enabled=" + enabled + "]";
+	}
+
+}
