472dd514e18b367e947813f9b5fa9874e6cd6385
caiotorrez <caiotorrez@live.com>
2018-11-03 15:08:16 -0300

    CustomUserDetails Created/Configured

---
+++ src/main/java/com/ipersonal/config/security/service/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.ipersonal.config.security.service;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ipersonal.model.Usuario;
+
+public class CustomUserDetails extends Usuario implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	private final String role;
+	
+	public CustomUserDetails(Usuario usuario) {
+		super(usuario);
+		this.role = this.getProfessor() != null ? "ROLE_ALUNO" : "ROLE_PROFESSOR";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList(this.role);
+	}
+	
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
